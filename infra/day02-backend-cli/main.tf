bfa290358de5b0a1a64ff7eed7a73c76d89b78dd
ehabSaad01 <38528868+ehabSaad01@users.noreply.github.com>
2025-10-15 17:51:26 +0200

    Update main.tf

--- infra/day02-backend-cli/main.tf
+++ infra/day02-backend-cli/main.tf
@@ -1,26 +1,19 @@
-terraform {
-  required_version = ">= 1.7.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
-  resource_provider_registrations = "none"
 }
 
-# Workload within RG scope (matches current RBAC)
-resource "azurerm_user_assigned_identity" "tfcli" {
-  name                = "uai-day02-tfcli"
-  resource_group_name = "rg-day02-backend-cli"
-  location            = "westeurope"
-  tags = {
-    project = "sogeti-ce"
-    origin  = "terraform"
-    track   = "cli"
-  }
+# Resource group reference
+data "azurerm_resource_group" "backend" {
+  name = "rg-day02-backend-cli"
+}
+
+# Create a simple User Assigned Managed Identity for test
+resource "azurerm_user_assigned_identity" "test_identity" {
+  name                = "uami-day02-test-cli"
+  location            = data.azurerm_resource_group.backend.location
+  resource_group_name = data.azurerm_resource_group.backend.name
+}
+
+output "test_identity_id" {
+  value = azurerm_user_assigned_identity.test_identity.id
 }
