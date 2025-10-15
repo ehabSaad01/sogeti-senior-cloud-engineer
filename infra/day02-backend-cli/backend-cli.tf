4e13bf39245dfd9a771ec6a7bbd5febb495a51e2
ehabSaad01 <38528868+ehabSaad01@users.noreply.github.com>
2025-10-15 17:48:41 +0200

    Update backend-cli.tf

--- infra/day02-backend-cli/backend-cli.tf
+++ infra/day02-backend-cli/backend-cli.tf
@@ -1,8 +1,13 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-day02-backend-cli"
-    storage_account_name = "stday02backendcliweu"
-    container_name       = "tfstate-cli"
-    key                  = "infra/primary.tfstate"
+    storage_account_name = "stday02tfstatecli"
+    container_name       = "tfstate"
+    key                  = "infra/day02-cli/terraform.tfstate"
+    use_azuread_auth     = true
   }
+}
+
+provider "azurerm" {
+  features {}
 }
