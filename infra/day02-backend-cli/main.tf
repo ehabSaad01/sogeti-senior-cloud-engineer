1ef92592bc08bfc4f4596a2e3cbfa9c20c4d095a
ehabSaad01 <38528868+ehabSaad01@users.noreply.github.com>
2025-10-16 11:34:29 +0200

    Update main.tf

--- infra/day02-backend-cli/main.tf
+++ infra/day02-backend-cli/main.tf
@@ -1,14 +1,18 @@
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
+
 data "azurerm_resource_group" "backend" {
   name = "rg-day02-backend-cli"
 }
+
 resource "azurerm_user_assigned_identity" "test_identity" {
   name                = "uami-day02-test-cli"
   location            = data.azurerm_resource_group.backend.location
   resource_group_name = data.azurerm_resource_group.backend.name
 }
+
 output "test_identity_id" {
   value = azurerm_user_assigned_identity.test_identity.id
 }
